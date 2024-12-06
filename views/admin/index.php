c86b579554a8c84731fcc9a83b68621a9bf7c8b3
shrwndave <otso8davebelleza@gmail.com>
2024-12-06 11:24:43 +0800

    add --all

--- views/admin/index.php
+++ views/admin/index.php
@@ -8,7 +8,7 @@
         <meta name="author" content="" />
         <title>CAR_MGMT_SYS - ADMIN/DASHBOARD</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
-        <link href="../views/css/styles.css" rel="stylesheet" />
+        <link href="../../assets/css/admin_styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     </head>
     <body class="sb-nav-fixed">
@@ -585,11 +585,11 @@
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="../views/js/scripts.js"></script>
+        <script src="../../assets/js/admin_scripts.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="../views/assets/demo/chart-area-demo.js"></script>
-        <script src="../views/assets/demo/chart-bar-demo.js"></script>
+        <script src="../../assets/demo/chart-area-demo.js"></script>
+        <script src="../../assets/demo/chart-bar-demo.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
-        <script src="../views/js/datatables-simple-demo.js"></script>
+        <script src="../../assets/datatables-simple-demo.js"></script>
     </body>
 </html>
